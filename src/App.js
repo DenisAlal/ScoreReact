feb0cbb29379755a8b80c195d4b57b4b202c64d4
Denis <denisalalykin20012gmail.com>
2022-10-18 11:10:16 +0700

    ScoreReact

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './index.scss';
+import React from 'react'
+function App() {
+  const [count, setCount] = React.useState(0);
+  const onClickPlus = () => {
+     setCount(count+1);
+  } 
+  const onClickMinus = () => {
+    if (count !== 0) {
+    setCount(count-1);}
+  } 
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+      <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>-</button>
+        <button className="plus" onClick={onClickPlus}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
